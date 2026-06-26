63f791e14288389e994831c505991a3b7f887f11
luchovs <lucianosavia15@gmail.com>
2026-06-26 14:30:58 -0300

    termine lo de script

--- formulario/script.js
+++ formulario/script.js
@@ -1,9 +1,21 @@
-const button = document.getElementById("click");
-
+const button = document.getElementById("button");
 
 button.addEventListener("click", ()=> {
-    console.log(input)
+    const nombre = document.getElementById("nombre").value
+    const direccion = document.getElementById("direccion").value
+    const telefono = document.getElementById("telefono").value
+    const simple = document.getElementById("simple").value
+    const doble = document.getElementById("doble").value
+    const triple = document.getElementById("triple").value
+    const bebida = document.getElementById("bebida").value
+    const cebolla = document.getElementById("cebolla").value
+    const huevo = document.getElementById("huevo").value
+    const bacon = document.getElementById("bacon").value
+    const cantidad = document.getElementById("cantidad").value
+    const comentarios = document.getElementById("comentarios").value
+    
 
 
-
+ console.log(nombre,direccion,telefono,simple,doble,triple,bebida,cebolla,huevo,bacon,cantidad,comentarios)
+    
 })
